1feb686ec98530902aeae6a01ba0d7cec486d8fc
Josh Comeau <joshwcomeau@gmail.com>
2018-07-28 08:34:36 -0400

    add GridVsWave and Toggle components

---
+++ src/components/GridVsWave/GridVsWave.js
@@ -0,0 +1,152 @@
+import React, { Component, Fragment } from 'react';
+import { Motion, spring } from 'react-motion';
+import styled from 'styled-components';
+
+import { COLORS } from '../../constants';
+import { convertProgressToOffset } from '../../helpers/waveform.helpers';
+
+import AirGrid from '../AirGrid';
+import Spacer from '../Spacer';
+import Waveform from '../Waveform';
+import WaveformCalculator from '../WaveformCalculator';
+import WaveformIntercept from '../WaveformIntercept';
+import AmplitudeFrequencyManager from '../AmplitudeFrequencyManager';
+import Toggle from '../Toggle';
+
+class GridVsWave extends Component {
+  state = {
+    highlightMolecule: false,
+    rotateWaveform: false,
+  };
+
+  render() {
+    const { highlightMolecule, rotateWaveform } = this.state;
+
+    return (
+      <Fragment>
+        <AmplitudeFrequencyManager>
+          {({ amplitude, frequency, progress }) => (
+            <Motion
+              style={{
+                frequency: spring(frequency),
+                amplitude: spring(amplitude),
+              }}
+            >
+              {({ frequency, amplitude }) => (
+                <Fragment>
+                  <Row>
+                    <AirGrid
+                      shape="sine"
+                      width={400}
+                      height={200}
+                      numOfCols={13}
+                      numOfRows={13}
+                      waveformAmplitude={
+                        // HACK: For some reason, the numOfRows/numOfCols affects
+                        // how tightly-bound they are
+                        amplitude * 0.5
+                      }
+                      waveformFrequency={frequency}
+                      waveformProgress={progress}
+                      highlightColumnIndex={highlightMolecule ? 0 : undefined}
+                    />
+
+                    <Spacer size={70} />
+
+                    <div
+                      style={{
+                        position: 'relative',
+                        transformOrigin: 'center center',
+                        transform: rotateWaveform
+                          ? 'rotate(90deg) translateX(-5%)'
+                          : 'rotate(0deg) translateX(0)',
+                        transition: 'transform 1000ms',
+                      }}
+                    >
+                      <WaveformCalculator
+                        width={350}
+                        height={175}
+                        frequency={frequency}
+                        amplitude={amplitude}
+                        progress={progress}
+                      >
+                        {points => (
+                          <Waveform
+                            width={350}
+                            height={175}
+                            points={points}
+                            color={
+                              highlightMolecule
+                                ? COLORS.gray[300]
+                                : COLORS.gray[900]
+                            }
+                          />
+                        )}
+                      </WaveformCalculator>
+                      {highlightMolecule && (
+                        <WaveformIntercept
+                          size={20}
+                          color={COLORS.primary[500]}
+                          waveformSize={350}
+                          waveformShape="sine"
+                          frequency={frequency}
+                          amplitude={amplitude}
+                          offset={convertProgressToOffset(progress)}
+                        />
+                      )}
+                    </div>
+                  </Row>
+                </Fragment>
+              )}
+            </Motion>
+          )}
+        </AmplitudeFrequencyManager>
+        <Row>
+          <Col>
+            <Toggle
+              isToggled={highlightMolecule}
+              onToggle={() =>
+                this.setState({
+                  highlightMolecule: !this.state.highlightMolecule,
+                })
+              }
+            />
+            <Spacer size={10} />
+            Highlight Molecule
+          </Col>
+          <Spacer size={70} />
+
+          <Col>
+            <Toggle
+              isToggled={rotateWaveform}
+              onToggle={() =>
+                this.setState({
+                  rotateWaveform: !this.state.rotateWaveform,
+                })
+              }
+            />
+            <Spacer size={10} />
+            Rotate Waveform
+          </Col>
+        </Row>
+      </Fragment>
+    );
+  }
+}
+
+const Row = styled.div`
+  display: flex;
+  width: 810px;
+  margin: auto;
+  margin-top: 35px;
+`;
+
+const Col = styled.div`
+  flex: 1;
+  text-align: left;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+`;
+
+export default GridVsWave;
---
+++ src/components/Toggle/Toggle.js
@@ -0,0 +1,158 @@
+// @flow
+import React, { PureComponent } from 'react';
+import styled, { keyframes } from 'styled-components';
+import { Motion, spring } from 'react-motion';
+
+import { COLORS } from '../../constants';
+
+type Props = {
+  isToggled: boolean,
+  size: number,
+  padding: number,
+  onToggle: (isToggled: boolean) => void,
+};
+
+class Toggle extends PureComponent<Props> {
+  static defaultProps = {
+    size: 32,
+    padding: 2,
+  };
+
+  lastTranslateVal: ?number = null;
+  lastFrameTime: ?number = null;
+
+  renderBall = ({ translate }: { translate: number }) => {
+    const { size } = this.props;
+
+    const { lastTranslateVal, lastFrameTime } = this;
+
+    if (lastTranslateVal == null || lastFrameTime == null) {
+      this.lastTranslateVal = translate;
+      this.lastFrameTime = performance.now();
+      return <Ball size={size} translate={translate} stretch={1} />;
+    }
+
+    const now = performance.now();
+
+    const translateDelta = Math.abs(lastTranslateVal - translate);
+    const timeDelta = now - lastFrameTime;
+
+    this.lastTranslateVal = translate;
+    this.lastFrameTime = now;
+
+    const timeAdjustment = 1 / (timeDelta / 16.666);
+
+    const stretch = translateDelta / 40;
+
+    return (
+      <Ball
+        size={size}
+        translate={translate}
+        stretch={1 + stretch * timeAdjustment}
+      />
+    );
+  };
+
+  render() {
+    const { isToggled, size, padding, onToggle } = this.props;
+    const doublePadding = padding * 2;
+
+    return (
+      <Wrapper
+        height={size + doublePadding}
+        width={size * 2 + doublePadding}
+        padding={padding}
+        onClick={() => onToggle(!isToggled)}
+      >
+        <OnBackground isVisible={isToggled}>
+          <Pulsing />
+        </OnBackground>
+        <Motion
+          style={{
+            translate: spring(isToggled ? 100 : 0, {
+              stiffness: 220,
+              damping: 19,
+            }),
+          }}
+        >
+          {this.renderBall}
+        </Motion>
+      </Wrapper>
+    );
+  }
+}
+
+const pulse = keyframes`
+  0% { opacity: 0 }
+  20% { opacity: 0 }
+  70% { opacity: 0.35 }
+  100% { opacity: 0 }
+`;
+
+const Wrapper = styled.button`
+  position: relative;
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+  padding: ${props => props.padding}px;
+  border: none;
+  border-radius: ${props => props.height / 2}px;
+  background-image: linear-gradient(
+    45deg,
+    ${COLORS.gray[400]},
+    ${COLORS.gray[200]}
+  );
+  overflow: hidden; /* Hide 'OnBackground' corners */
+  outline: none; /* TODO: better a11y story */
+  box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+`;
+
+const OnBackground = styled.div`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: linear-gradient(
+    15deg,
+    ${COLORS.blue[700]},
+    ${COLORS.indigo[500]}
+  );
+  opacity: ${props => (props.isVisible ? 1 : 0)};
+  transition: opacity 300ms;
+  box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.2);
+`;
+
+const Pulsing = styled.div`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: ${COLORS.blue[800]};
+  animation: ${pulse} 2000ms infinite;
+  box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.2);
+`;
+
+const Ball = styled.div.attrs({
+  style: props => ({
+    transform: `
+      translateX(${props.translate}%)
+      scaleX(${props.stretch})
+    `,
+  }),
+})`
+  position: relative;
+  z-index: 2;
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  background: ${COLORS.white};
+  border-radius: 50%;
+
+  transform-origin: center center;
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.2);
+`;
+
+export default Toggle;
